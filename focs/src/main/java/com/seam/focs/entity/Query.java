52ce1e05228d180c5c2c669809b032c8d29b4a2e
SheeYeap02 <sheeyeap0608@gmail.com>
2023-09-21 22:48:03 +0800

    solve

--- focs/src/main/java/com/seam/focs/entity/Query.java
+++ focs/src/main/java/com/seam/focs/entity/Query.java
@@ -1,10 +1,12 @@
 package com.seam.focs.entity;
 import com.baomidou.mybatisplus.annotation.TableId;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Query {
-    private Long applicantId;
+public class Query implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @TableId
     private Long queryId;
     private String title;
@@ -13,6 +15,7 @@ public class Query {
     private String queryStatus;
     private Date created;
     private Date completed;
+    private Long applicantId;
 
     public Query() {
         // Default constructor
@@ -29,5 +32,67 @@ public class Query {
         this.completed = completed;
     }
 
+    public Long getQueryId() {
+        return queryId;
+    }
 
+    public void setQueryId(Long queryId) {
+        this.queryId = queryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public String getQueryStatus() {
+        return queryStatus;
+    }
+
+    public void setQueryStatus(String queryStatus) {
+        this.queryStatus = queryStatus;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Date completed) {
+        this.completed = completed;
+    }
+
+    public Long getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(Long applicantId) {
+        this.applicantId = applicantId;
+    }
 }
