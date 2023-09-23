6479601aa8b3b5939ed164273d7c91800a12bce5
Lee0JJ <ace010110@gmail.com>
2023-09-23 18:56:37 +0800

    ip to footer
    ip to footer

--- focs/src/main/resources/dist/focs/js/headerAppend.js
+++ focs/src/main/resources/dist/focs/js/headerAppend.js
@@ -14,7 +14,7 @@
 //         .catch(error => console.error('Error fetching header.html:', error));
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Fetch the content of header.html
     fetch('header.html')
         .then(response => response.text())
@@ -60,11 +60,36 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(headerHTML => {
             // Find the element with class "headerjs"
             var headerElement = document.querySelector(".footerjs");
+            const parts = navigator.userAgent.split('/');
+
+            fetch('https://api.ipify.org/?format=json')
+                .then(results => results.json())
+                .then(data => {
+                    // const ipAddress = data.ip;
+                    // const replacedUserAgent = parts.map(part => part === '127.0.0.1' ? ipAddress : part).join('/');
+
+                    console.log(data.ip + '\n' + parts[0] + '\n' + parts[1]);
+
+                    const ipinfo = "abc";
+                    headerHTML = headerHTML.replace(/127\.0\.0\.1/g, parts[0] + ': ' + data.ip );
+
+                    // Now you can use `replacedUserAgent` as needed
+                    //console.log(replacedUserAgent);
+
+                    // If you want to update an HTML element with the modified user agent
+                    //document.getElementById('ipAdd').innerText = replacedUserAgent;
+
+                    // Replace its innerHTML with the fetched headerHTML
+                    if (headerElement) {
+                        headerElement.innerHTML = headerHTML;
+                    }
+                })
+                .catch(error => console.error('Error fetching IP address:', error));
 
             // Replace its innerHTML with the fetched headerHTML
-            if (headerElement) {
-                headerElement.innerHTML = headerHTML;
-            }
+            // if (headerElement) {
+            //     headerElement.innerHTML = headerHTML;
+            // }
         })
         .catch(error => console.error('Error fetching footer.html:', error));
 });
