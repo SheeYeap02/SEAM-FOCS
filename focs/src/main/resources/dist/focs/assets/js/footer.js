44211b7ae41f5e9dce49d42a13b5e10b827cf4e0
Yenny <92930651+yenny1o15@users.noreply.github.com>
2023-09-24 19:22:26 +0800

    UI Version 2

---
+++ focs/src/main/resources/dist/focs/assets/js/footer.js
@@ -0,0 +1,65 @@
+//create footer element and append to body
+var footer = document.createElement("footer");
+
+const parts = navigator.userAgent.split('/');
+
+
+
+footer.innerHTML = `   <footer id="footer">
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6">
+        <img src="img/footer1.jpg" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-3 col-md-6">
+        <div class="footer-info">
+          <h3>Address</h3>
+          <p><strong>Faculty of Computing and Information Technology</strong> <br>
+            Tunku Abdul Rahman University of Management and Technology <br>
+            Jalan Genting Kelang, Setapak <br>
+            53300 Kuala Lumpur <br>
+            Malaysia <br><br>
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Contact</h4>
+        <ul>
+          <li><strong>Phone: </strong> 603-41450123 Ext no. 3233</li>
+          <li><strong>Phone: </strong> <a class="text-primary-hover"
+              href="mailto:contact@sampledomain.com">603-41423166 / 018 925 1001</a></li>
+          <li><strong>Email: </strong> <a class="text-primary-hover"
+              href="mailto:focs@tarc.edu.my">focs@tarc.edu.my</a></li>
+          <li><strong>Your IP Address: </strong> 127.0.0.1</li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <div class="footer-info">
+          <h3>Where We Are?</h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.53781216072!2d101.72217973488769!3d3.2152552000000023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc3843bfb6a031%3A0x2dc5e067aae3ab84!2sTunku%20Abdul%20Rahman%20University%20of%20Management%20and%20Technology%20(TAR%20UMT)!5e0!3m2!1sen!2smy!4v1694823435275!5m2!1sen!2smy"
+            frameborder="0" style="border:0; width: 100%; height: 270px;" allowfullscreen></iframe>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="copyright">
+    TAR UMT © 2023 All Rights Reserved Disclaimer Privacy Policy and ABAC Policy
+  </div>
+</div>
+</footer>`;
+
+
+
+
+//append footer to the last element in body
+document.body.appendChild(footer);
---
+++ focs/src/main/resources/dist/focs/assets/js/header.js
@@ -0,0 +1,32 @@
+//create header element and append to body
+var header = document.createElement("header");
+header.innerHTML = ` <header id="header" class="fixed-top d-flex align-items-center">
+<div class="container d-flex align-items-center">
+
+  <div class="logo me-auto">
+    <h1><a href="homePage.html"><img src="https://www.tarc.edu.my/images/tarumt-logo1.png?v=beyongEducation2"
+          alt="TAR UC" /></a></h1>
+  </div>
+
+  <nav id="navbar" class="navbar order-last order-lg-0">
+    <ul>
+      <li><a class="nav-link scrollto" href="homePage.html#">Home</a></li>
+      <li class="dropdown"><a href="#"><span>Programme</span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a class="nav-link scrollto" href="programmes2.html">All Programme</a></li>
+          <li><a class="nav-link scrollto" href="programmes2.html#searchProgramme">Diploma Programme</a></li>
+          <li><a class="nav-link scrollto" href="programmes2.html#searchProgramme">Bachelor Programme</a></li>
+        </ul>
+      </li>
+      <li><a class="nav-link scrollto" href="aboutus2.html">About Us</a></li>
+      <li><a class="nav-link scrollto" href="stafftry2.html">Our Team</a></li>
+      <li><a class="nav-link scrollto" href="facili2.html">Facilities</a></li>
+      <li><a class="nav-link scrollto" href="submitEnquiry2.html">Contact</a></li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+
+</div>
+</header>`;
+//append header to the first element in body
+document.body.insertBefore(header, document.body.firstChild);
